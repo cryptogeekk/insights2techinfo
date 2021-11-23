d23218c1055916dada86e0d64cba9e74b7872792
cryptogeekk <52788412+cryptogeekk@users.noreply.github.com>
2021-11-23 23:33:09 +0530

    update

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import pandas as pd 
+from datetime import datetime
+from string import Template
 
 def print_output_html():
     data=pd.read_csv('data.csv')
@@ -8,20 +10,29 @@ def print_output_html():
 
     pd.set_option('colheader_justify', 'center')   # FOR TABLE <th>
 
-    html_string = """
+    time_updated = datetime.utcnow()
+
+
+    html_string = """\
     <html>
-    <head><title>HTML Pandas Dataframe with CSS</title></head>
+    <head><title>Researcher List</title>
+    <h1>Highly Cited Computer Science Researcher According to WoS 2021</h1>
+    <p>Dataset last updated on $code</p>
+    <p>The original list published by WoS only contains few metrics such as name, affillation. 
+    But in our database we have used ten different metrics to make researchers information more informative.
+    </p>
+    </head>
     <link rel="stylesheet" type="text/css" href="df_style.css"/>
     <body>
         {table}
     </body>
     </html>.
     """
-
+    string_html1=Template(html_string).safe_substitute(code=time_updated)
 
     # OUTPUT AN HTML FILE
     with open('output.html', 'w') as f:
-        f.write(html_string.format(table=data.to_html(classes='mystyle')))
+        f.write(string_html1.format(table=data.to_html(classes='mystyle')))
     
 if __name__=="__main__":
     print_output_html()
