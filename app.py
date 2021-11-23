869d361264174e7db2aa787a4c462f2113b01203
Krishna Yadav <52788412+cryptogeekk@users.noreply.github.com>
2021-11-23 19:13:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 def print_output_html():
     data=pd.read_csv('data.csv')
+    columns=data.columns
+    data.drop([columns[0]], axis=1, inplace=True)
     data.to_html('output.html')
 
 if __name__=="__main__":
