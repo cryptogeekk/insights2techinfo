22d4770a0edbaf29d9e1c125d472d5305f8dcfcc
cryptogeekk <52788412+cryptogeekk@users.noreply.github.com>
2021-11-23 21:47:05 +0530

    update

--- app.py
+++ app.py
@@ -6,5 +6,22 @@ def print_output_html():
     data.drop([columns[0]], axis=1, inplace=True)
     data.to_html('output.html')
 
+    pd.set_option('colheader_justify', 'center')   # FOR TABLE <th>
+
+    html_string = """
+    <html>
+    <head><title>HTML Pandas Dataframe with CSS</title></head>
+    <link rel="stylesheet" type="text/css" href="df_style.css"/>
+    <body>
+        {table}
+    </body>
+    </html>.
+    """
+
+
+    # OUTPUT AN HTML FILE
+    with open('output.html', 'w') as f:
+        f.write(html_string.format(table=data.to_html(classes='mystyle')))
+    
 if __name__=="__main__":
     print_output_html()
